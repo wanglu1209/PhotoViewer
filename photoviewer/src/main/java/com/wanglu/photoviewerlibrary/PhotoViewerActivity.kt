e5e4a6bbe7b93983f77fe97ce11e5cd0848b6826
王璐 <2021765485@qq.com>
2018-08-09 16:58:29 +0800

    增加指示器

--- photoviewer/src/main/java/com/wanglu/photoviewerlibrary/PhotoViewerActivity.kt
+++ photoviewer/src/main/java/com/wanglu/photoviewerlibrary/PhotoViewerActivity.kt
@@ -5,8 +5,12 @@ import android.support.v4.view.PagerAdapter
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
 import com.wanglu.photoviewerlibrary.PhotoViewer.PIC_DATA
 import com.wanglu.photoviewerlibrary.PhotoViewer.mInterface
 import com.wanglu.photoviewerlibrary.Utils.dp2px
@@ -29,6 +33,25 @@ class PhotoViewerActivity : AppCompatActivity() {
     var mTotalPage: Int = 0
     var mClickViewLocation: IntArray? = null
     var mClickLocation = IntArray(2)
+
+    /**
+     * 小圆点的drawable
+     * 下标0的为没有被选中的
+     * 下标1的为已经被选中的
+     */
+    private val mDot = intArrayOf(R.drawable.no_selected_dot, R.drawable.selected_dot)
+    /**
+     * 存放小圆点的Group
+     */
+    private var mDotGroup: LinearLayout? = null
+    /**
+     * 存放没有被选中的小圆点Group和已经被选中小圆点
+     */
+    private var mFrameLayout: FrameLayout? = null
+    /**
+     * 选中的小圆点
+     */
+    private var mSelectedDot: View? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,17 +90,30 @@ class PhotoViewerActivity : AppCompatActivity() {
         mLookPicVP.adapter = adapter
         mLookPicVP.currentItem = mCurrentPage
 
+        var p = 0
 
         mLookPicVP.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrollStateChanged(state: Int) {
-
             }
 
             override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+                if (mSelectedDot != null) {
+                    val dx = mDotGroup!!.getChildAt(1).x - mDotGroup!!.getChildAt(0).x
+                    if (position != mCurrentPage) {
+                        p += position - mCurrentPage
+                        mCurrentPage = position
+                    }
+                    mSelectedDot!!.translationX = p * dx + positionOffset * dx
+                }
             }
 
             override fun onPageSelected(position: Int) {
-
+//                if (mSelectedDot != null) {
+//                    val dx = mDotGroup!!.getChildAt(1).x - mDotGroup!!.getChildAt(0).x
+//                    mSelectedDot!!.translationX = position * dx
+//                    Log.d("112233", "selected --- $position --- ${mSelectedDot!!.translationX }")
+//                }
                 mClickLocation[0] = (position % mCountRow) * mWidth + (position % mCountRow) * mLeftSpace + mClickViewLocation!![0]
                 mClickLocation[1] = position / mCountRow * mHeight + position / mCountRow * mTopSpace + mClickViewLocation!![1]
 
@@ -85,6 +121,82 @@ class PhotoViewerActivity : AppCompatActivity() {
             }
 
         })
+
+
+
+
+
+
+
+
+        root.post {
+
+            /**
+             * 实例化两个Group
+             */
+            mFrameLayout = FrameLayout(this)
+            mDotGroup = LinearLayout(this)
+
+            if (mDotGroup!!.childCount != 0)
+                mDotGroup!!.removeAllViews()
+            val dotParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT)
+            /**
+             * 未选中小圆点的间距
+             */
+            dotParams.rightMargin = dp2px(this, 12)
+
+            /**
+             * 创建未选中的小圆点
+             */
+            for (i in 0 until mPicData!!.size) {
+                val iv = ImageView(this)
+                iv.setImageDrawable(resources.getDrawable(mDot[0]))
+                iv.layoutParams = dotParams
+                mDotGroup!!.addView(iv)
+            }
+
+            /**
+             * 设置小圆点Group的方向为水平
+             */
+            mDotGroup!!.orientation = LinearLayout.HORIZONTAL
+            /**
+             * 设置小圆点在中间
+             */
+            mDotGroup!!.gravity = Gravity.CENTER or Gravity.BOTTOM
+            /**
+             * 两个Group的大小都为match_parent
+             */
+            val params = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT)
+
+
+            params.bottomMargin = dp2px(this, 20)
+            /**
+             * 首先添加小圆点的Group
+             */
+            root.addView(mDotGroup, params)
+
+            mDotGroup!!.post {
+
+                if (mSelectedDot == null) {
+                    val iv = ImageView(this)
+                    iv.setImageDrawable(resources.getDrawable(mDot[1]))
+                    val params = FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                    /**
+                     * 设置选中小圆点的左边距
+                     */
+                    params.leftMargin = mDotGroup!!.getChildAt(0).x.toInt() + dotParams.rightMargin * mCurrentPage + mDotGroup!!.getChildAt(0).width * mCurrentPage
+                    params.gravity = Gravity.BOTTOM
+                    mFrameLayout!!.addView(iv, params)
+                    mSelectedDot = iv
+                }
+                /**
+                 * 然后添加包含未选中圆点和选中圆点的Group
+                 */
+                root.addView(mFrameLayout, params)
+            }
+        }
 
     }
 
@@ -124,10 +236,10 @@ class PhotoViewerActivity : AppCompatActivity() {
                 intAlpha -= dy.toInt()
                 if (alpha > 1) alpha = 1f
                 else if (alpha < 0) alpha = 0f
-                if(intAlpha < 0) intAlpha = 0
+                if (intAlpha < 0) intAlpha = 0
                 else if (intAlpha > 255) intAlpha = 255
                 root.background.alpha = intAlpha    // 更改透明度
-                if(alpha >= 0.6)
+                if (alpha >= 0.6)
                     iv.attacher.scale = alpha   // 更改大小
 
 
@@ -152,7 +264,7 @@ class PhotoViewerActivity : AppCompatActivity() {
 
 
     override fun onPause() {
-        overridePendingTransition(0,0)
+        overridePendingTransition(0, 0)
         super.onPause()
     }
 
